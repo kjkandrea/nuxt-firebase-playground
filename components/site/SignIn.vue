b21396257c36fc0d0641bb7fe4a8f4a69008ba5b
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-28 12:57:04 +0900

    Signin 컴포넌트 생성

---
+++ components/site/SignIn.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <v-btn icon @click.stop="dialog = true">
+      <v-icon>
+        mdi-account
+      </v-icon>
+    </v-btn>
+    <v-dialog
+      v-model="dialog"
+      max-width="380"
+    >
+      <v-card>
+        <v-card-title class="headline mb-5">
+          로그인
+          <v-spacer />
+          <v-btn
+            icon
+            @click.stop="dialog = false"
+          >
+            <v-icon>
+              mdi-close
+            </v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              name="email"
+              type="email"
+              placeholder="이메일을 입력하세요."
+              outlined
+              hide-details
+              class="mb-3"
+            />
+
+            <v-text-field
+              name="password"
+              type="password"
+              placeholder="비밀번호를 입력하세요."
+              outlined
+              hide-details
+              class="mb-3"
+            />
+            <v-btn
+              type="submit"
+              color="primary"
+              block
+              x-large
+            >
+              로그인 하기
+            </v-btn>
+          </v-form>
+        </v-card-text>
+
+        <v-card-actions class="other-auth flex-column">
+          <p>또는 다음의 방식으로 이용 가능 합니다.</p>
+          <v-btn
+            block
+            rounded
+            outlined
+            large
+          >
+            <v-icon class="mr-2">mdi-google</v-icon>
+            Google 계정으로 이용하기
+          </v-btn>
+          <v-btn
+            block
+            rounded
+            outlined
+            large
+          >
+            <v-icon class="mr-2">mdi-facebook</v-icon>
+            Facebook 계정으로 이용하기
+          </v-btn>
+          <v-btn
+            block
+            rounded
+            outlined
+            large
+          >
+            <v-icon class="mr-2">mdi-github</v-icon>
+            Github 계정으로 이용하기
+          </v-btn>
+          <v-btn
+            block
+            rounded
+            outlined
+            large
+          >
+            <v-icon class="mr-2">mdi-apple</v-icon>
+            Apple 계정으로 이용하기
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dialog: true
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,118 +1,130 @@
 <template>
-  <v-app dark>
+  <v-app id="keep">
+    <v-app-bar
+      app
+      clipped-left
+      color="white"
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <span class="title ml-3 mr-5">Google&nbsp;<span class="font-weight-light">Keep</span></span>
+
+      <v-spacer />
+      <sign-in />
+    </v-app-bar>
+
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
       app
+      clipped
+      color="grey lighten-4"
     >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
+      <v-list
+        dense
+        class="grey lighten-4"
+      >
+        <template v-for="(item, i) in items">
+          <v-row
+            v-if="item.heading"
+            :key="i"
+            align="center"
+          >
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-col>
+            <v-col
+              cols="6"
+              class="text-right"
+            >
+              <v-btn
+                small
+                text
+              >
+                edit
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-divider
+            v-else-if="item.divider"
+            :key="i"
+            dark
+            class="my-4"
+          />
+          <v-list-item
+            v-else
+            :key="i"
+            link
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title class="grey--text">
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container
+        fluid
+        class="grey lighten-4 fill-height"
+      >
+        <v-row
+          justify="center"
+          align="center"
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-content>
-      <v-container>
-        <nuxt />
+          <v-col class="shrink">
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  large
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon large>
+                    mdi-code-tags
+                  </v-icon>
+                </v-btn>
+              </template>
+              <nuxt />
+            </v-tooltip>
+          </v-col>
+        </v-row>
       </v-container>
-    </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import SignIn from '~/components/site/SignIn.vue'
+
 export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
+  components: {
+    SignIn
+  },
+  data: () => ({
+    drawer: null,
+    items: [
+      { icon: 'mdi-home', text: 'Notes' },
+      { icon: 'mdi-home', text: 'Reminders' },
+      { divider: true },
+      { heading: 'Labels' },
+      { icon: 'mdi-home', text: 'Create new label' },
+      { divider: true },
+      { icon: 'mdi-home', text: 'Archive' },
+      { icon: 'mdi-home', text: 'Trash' },
+      { divider: true },
+      { icon: 'mdi-home', text: 'Settings' },
+      { icon: 'mdi-home', text: 'Trash' },
+      { icon: 'mdi-home', text: 'Help' }
+    ]
+  })
 }
 </script>
 
